ff369bb6fdd2b97fc2a6e0fa768c07187c19a727
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-04-07 23:30:23 +0530

    dj

--- mysql_pro/app/models.py
+++ mysql_pro/app/models.py
@@ -8,3 +8,6 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     std = models.IntegerField()
     gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.name} | ({self.sid+1})"
