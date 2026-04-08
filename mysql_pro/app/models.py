06f1cf6ba4e71b7193cf047485f72b8edb21c807
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-04-08 16:55:33 +0530

    dj

--- mysql_pro/app/models.py
+++ mysql_pro/app/models.py
@@ -11,3 +11,13 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.name} | ({self.sid+1})"
+
+
+class Emp(models.Model):
+    name = models.CharField(max_length=100)
+    job = models.CharField(max_length=100)
+    sal = models.IntegerField(default=0)
+    deptno = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} | ({self.pk})"
